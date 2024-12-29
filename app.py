ee40506889a7ecad02f2bc5a7ada9d709fac6eda
Konstantin <k.dilyanov@gmail.com>
2024-12-29 20:15:31 +0000

    Added a table to view all boxes and their contents in a list, also added a form to add new boxes

--- app.py
+++ app.py
@@ -16,40 +16,53 @@ app.config
 # initialize the app with the extension
 db = SQLAlchemy(app)
 
-class Item(db.Model):
-	id=db.Column(db.Integer, primary_key=True)
-	box=db.Column(db.String(50), nullable=False)
-	name=db.Column(db.String(50), nullable=False)
-	quantity=db.Column(db.Integer, nullable=False, default=1)
-	added_on=db.Column(db.DateTime, default=datetime.utcnow)
 
-
-	def __repr__(self) -> str:
-		return f"Item {self.id}"
-	
 class Box(db.Model):
+	
 	id=db.Column(db.Integer, primary_key=True)
-	box_name=db.Column(db.String(50), nullable=False)
-	item_id=db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+	title=db.Column(db.String(50), nullable=False)
+	comment=db.Column(db.String(150), nullable=True)
+	
+	items=db.relationship('Item',back_populates='box', cascade='all, delete-orphan')
 	
 
 	def __repr__(self) -> str:
 		return f"Box {self.id}"
+
+class Item(db.Model):
 	
-with app.app_context():
-		db.create_all()
+	id=db.Column(db.Integer, primary_key=True)
+	name=db.Column(db.String(50), nullable=False)
+	quantity=db.Column(db.Integer, nullable=False, default=1)
+	added_on=db.Column(db.DateTime, default=datetime.utcnow)
+
+	box_id = db.Column(db.Integer, db.ForeignKey('box.id'), nullable=False)
+	box = db.relationship('Box', back_populates='items')
+
+
+	def __repr__(self) -> str:
+		return f"Item {self.name}"
+	
+
+	
+
 # Home page
 @app.route("/",methods=['POST','GET'])
 def index():
 	# add item to box
 	if request.method == 'POST':
-		current_item = request.form['item']
-		current_box = request.form['box']
+		add_item = request.form['item']
+		add_box = request.form['box']
 		item_quantity = request.form['quantity']
-		
-		new_item = Item(box=current_box,name=current_item,quantity=item_quantity)
+		new_box = request.form['title']
+		new_comment = request.form['comment']
+		print(f"Received title: {new_box}, comment: {new_comment}")
+		new_item = Item(box_id=add_box,name=add_item,quantity=item_quantity)
+		add_box = Box(title=new_box,comment=new_comment)
+
 		try:
 			db.session.add(new_item)
+			db.session.add(add_box)
 			db.session.commit()
 			return redirect('/')
 		except Exception as e:
@@ -58,8 +71,9 @@ def index():
 
 	# See all items from the box
 	else:
-		all_items = Item.query.order_by(Item.box).all()
-		return render_template('index.html',all_items=all_items)
+		all_items = Item.query.order_by(Item.id).all()
+		all_boxes = Box.query.order_by(Box.id).all()
+		return render_template('index.html',all_boxes=all_boxes, all_items=all_items)
 
 
 # Delete item from the box
@@ -89,8 +103,11 @@ def edit(id:int):
 			return f"This error {e} occured"
 	else:
 		return render_template('edit.html',edit_item=edit_item)
+	
+
 
 
 if __name__ == "__main__":
-	
+	with app.app_context():
+		db.create_all()
 	app.run(debug=True,port=5004)
